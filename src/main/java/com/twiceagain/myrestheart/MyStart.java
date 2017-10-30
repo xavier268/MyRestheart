0feb6de3408c25d33622b3159d53cb4d4e834113
Xavier(fedora26.vbox) <xavier@nospamplease>
2017-10-30 22:31:29 +0100

    Extracting embedded config files.

--- src/main/java/com/twiceagain/myrestheart/MyStart.java
+++ src/main/java/com/twiceagain/myrestheart/MyStart.java
@@ -8,6 +8,9 @@ package com.twiceagain.myrestheart;
 import static com.twiceagain.myrestheart.utils.MyUtils.copyResourceToFile;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.restheart.Bootstrapper;
 import org.restheart.ConfigurationException;
 
@@ -15,7 +18,6 @@ import org.restheart.ConfigurationException;
  *
  * @author xavier
  */
-
 public class MyStart {
 
     /**
@@ -25,15 +27,23 @@ public class MyStart {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws ConfigurationException, URISyntaxException, IOException {
-        
+
         // Identify custom vs. standard version.
         System.out.println("\nStarting my customized bootstrapper (MyRestheart) ...");
-        
-        // Copy embedded configuration files to filesystem.
-        copyResourceToFile("myconfig.yaml","configuration.yaml");
-        
-        
-        Bootstrapper.main(args);
+
+        // If no config file specified on command line,  add the embedded one.
+        List<String> newArgs = new ArrayList<>(Arrays.asList(args));
+        if ((newArgs.contains("--fork") && newArgs.size() == 1)
+                || newArgs.isEmpty()) {
+            System.out.println("Extracting and using embedded configuration file");
+            String security = copyResourceToFile("security.yml", "security.yml");
+            String configuration = copyResourceToFile("mydefaultconfig.yml", "myconfiguration.yml");
+            
+        } else {
+            System.out.println("Using provided command line arguments");
+        }
+
+        Bootstrapper.main((String[]) newArgs.toArray(args));
 
     }
 
--- src/main/java/com/twiceagain/myrestheart/utils/MyUtils.java
+++ src/main/java/com/twiceagain/myrestheart/utils/MyUtils.java
@@ -8,39 +8,91 @@ package com.twiceagain.myrestheart.utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Various utilities.
+ *
  * @author xavier
  */
 public class MyUtils {
-    
+
     /**
-     * Save embedded resource file locally in default filesytem.
-     * Actual location will depend from wheteer we are running as jar (cli) or folder (netbeans).
-     * Existing file will be replaced, if any.
+     * Save embedded resource file locally in default filesytem. Actual location
+     * will depend from wheteer we are running as jar (cli) or folder
+     * (netbeans). Existing file will be replaced, if any.
+     *
      * @param resourceName relative to the root jar directory.
-     * @param fileName 
-     * @return  absolute file name of the saved resource.
+     * @param fileName
+     * @return absolute file name of the saved resource.
      */
-    static public String  copyResourceToFile(String resourceName, String fileName) {
-        
+    static public String copyResourceToFile(String resourceName, String fileName) {
         // Copy embedded custom configuration file locally.
         InputStream is = ClassLoader.getSystemResourceAsStream(resourceName);
         Path target = Paths.get(fileName);
         try {
             Files.copy(is, target, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException ex) {
-            Logger.getLogger(MyUtils.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(Arrays.toString(ex.getStackTrace()));
             return null;
         }
         return target.toAbsolutePath().toString();
+    }
+
+    /**
+     * Save embedded resource file locally in default filesytem. Actual location
+     * will depend from wheteer we are running as jar (cli) or folder
+     * (netbeans). Existing file will be replaced, if any.
+     *
+     * @param resourceName relative to the root jar directory. Will be used as
+     * the local target file name.
+     * @return absolute file name of the saved resource.
+     */
+    static public String copyResourceToFile(String resourceName) {
+        return copyResourceToFile(resourceName, resourceName);
+    }
+
+    /**
+     * Extract all necessary embeded ressources from file. The parameters are 
+     * adjusted as needed, with the absolute file names.
+     *
+     * @return absolute file name of the main configuration file.
+     */
+    static public String extractAndAdjustEmbedded() {
+
+        try {
+            String configure = copyResourceToFile("mydefaultconfig.yml");
+            String security = copyResourceToFile("security.yml");
+            
+            // Substitute absolute path names.
+            List<String> newConf = Files.lines(Paths.get(configure))
+                    .map((line) -> {
+                        line =  line.replaceAll("security.yml", security);                        
+                        return line;
+                    })
+                    .collect(Collectors.toList());
+            
+            // Rewrite modified file.
+            Files.write(Paths.get(configure),newConf, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+                   
+            return configure;
+        } catch (IOException ex) {
+            Logger.getLogger(MyUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
         
     }
-    
+
 }
