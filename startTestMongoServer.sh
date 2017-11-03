6519fffa0931a671f2b2a931e4d5898b8a3cbd19
Xavier(fedora26.vbox) <xavier@nospamplease>
2017-11-03 11:29:44 +0100

    Stack config

--- startTestMongoServer.sh
+++ startTestMongoServer.sh
@@ -3,7 +3,6 @@
 echo "Starts a test mongo server locally"
 echo "Type Ctrl-C to stop and destroy the test container"
 
-docker rm test-mongo
 docker run --rm -it -p27017:27017 mvertes/alpine-mongo
 
 
